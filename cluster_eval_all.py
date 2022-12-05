8c65dc4881fe8b147776c462c01e4bb900a79285
Rodrigo Morais <rmoraisu@protonmail.com>
2022-12-05 15:13:26 -0600

    cluster evaluation all

--- cluster_eval_all.py
+++ cluster_eval_all.py
@@ -1,4 +1,18 @@
 from clustering import cluster_df
 
-def cluster_eval(cluster_df):
+def cluster_eval(df):
+  max_days = 15
+  num_clusters = len(df['cluster'].unique())
+  step_size = num_clusters / max_days
+  ranges = [{'min': i * step_size, 'max': (i + 1) * step_size} for i in range(num_clusters)]
+  df['wrong_cluster'] = df.apply(lambda entry: entry['days_since'] in range(ranges[entry['cluster']]['min'], ranges[entry['cluster']]['max']))
   
+  total_count = len(df)
+  total_incorrect = len(df[~df['wrong_cluster']])
+
+  return total_incorrect / total_count
+
+
+if __name__ == '__main__':
+  df = cluster_df()
+  cluster_eval(df)
