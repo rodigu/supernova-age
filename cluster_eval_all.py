89cde0fa0d10156d0f1f12a69ea795f1c6daa64b
talizacks <55198700+talizacks@users.noreply.github.com>
2022-12-05 21:44:39 -0600

    created prelim heatmaps

--- cluster_eval_all.py
+++ cluster_eval_all.py
@@ -1,10 +1,12 @@
 from clustering import cluster_df
+import pandas as pd
+import matplotlib.pyplot as plt
 
 def cluster_eval(df):
   max_days = 15
   num_clusters = len(df['cluster'].unique())
   step_size = int(max_days/ num_clusters)
-  ranges = [{'min': i * step_size, 'max': (i + 1) * step_size} for i in range(num_clusters)]
+  ranges = [{'min': i * step_size, 'max': (i + 1) * step_size} for i in range(num_clusters)] #why can't we do just 3 ranges?
   cluster_proportions = { cluster: [0] * len(ranges) for cluster in range(num_clusters) }
   print(ranges)
   for ck in cluster_proportions.keys(): # for each cluster
@@ -18,12 +20,29 @@ def cluster_eval(df):
 
 
 if __name__ == '__main__':
-  df = cluster_df(filename='./out/output_1.csv', num_clusters=3)
-  print(df)
+  filename='two_day_range.csv'
+  num_clusters=4
+  df = cluster_df(filename, num_clusters)
+  # print(df)
   evaluation = cluster_eval(df)
+  all_cluster_ratio_list = []
+  print(len(list(evaluation.keys())))
   for c in evaluation.keys():
     total = sum(evaluation[c])
     print(f'Cluster: {c}')
+    cluster_ratio_list = []
     for i, count in enumerate(evaluation[c]):
-      print('range  proportion')
+      print('range       proportion')
       print(f'{i}      {count / total}')
+
+      ratio = count / total
+      cluster_ratio_list.append(ratio)
+    all_cluster_ratio_list.append(cluster_ratio_list)
+      # print(cluster_ratio_dict[c][i])
+  # cluster_ratio_df = 
+  print([i for i in all_cluster_ratio_list])
+  cluster_ratio_df = pd.DataFrame(all_cluster_ratio_list,columns=evaluation.keys())
+  print(cluster_ratio_df)
+  plt.imshow(cluster_ratio_df)
+  plt.title(f'{num_clusters} Clusters, {filename}')
+  plt.show()
