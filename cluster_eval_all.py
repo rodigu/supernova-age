eddcb4491ebb11202c2761a7be4614ecde0ca079
talizacks <55198700+talizacks@users.noreply.github.com>
2022-12-07 12:32:33 -0600

    before presentation corrections

--- cluster_eval_all.py
+++ cluster_eval_all.py
@@ -1,64 +1,89 @@
 from clustering import cluster_df
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 def cluster_eval(df):
   max_days = 15
   num_clusters = len(df['cluster'].unique())
-  step_size = int(max_days/ num_clusters)
+  step_size = int(max_days / num_clusters)
   ranges = [{'min': i * step_size, 'max': (i + 1) * step_size} for i in range(num_clusters)]
+  print(num_clusters)
   cluster_proportions = { cluster: [0] * len(ranges) for cluster in range(num_clusters) }
-  for ck in cluster_proportions.keys(): # for each cluster
-    for idx, r in enumerate(ranges): # for each possible range
-      # we check how many that are in this cluster and fit the range
-      # print(df[df['cluster'] == ck])
-      cluster_proportions[ck][idx] = len(df[(df['cluster'] == ck) & df['days_since'].between(r['min'], r['max'])])
-      
-    # df['wrong_cluster'] = df.apply(lambda entry: entry in range(ranges[entry['cluster']]['min'], ranges[entry['cluster']]['max']), axis=1)
-  
-  all_cluster_ratio_list = [cluster_proportions[c] for c in cluster_proportions.keys()]
-  cluster_ratio_df = pd.DataFrame(all_cluster_ratio_list,columns=cluster_proportions.keys(),index = ranges)
+  for idx, r in enumerate(ranges):
+    for cluster in cluster_proportions.keys(): # for each cluster
+      # print(cluster, r)
+      is_cluster = df['cluster'] == cluster
+      is_in_range = df['days_since'].between(r['min'], r['max'])
+      cluster_proportions[cluster][idx] = len(df[is_cluster & is_in_range])
+      # print(len(df[is_cluster & is_in_range]))
+
+  all_cluster_ratio_list = np.array([cluster_proportions[c] for c in cluster_proportions.keys()]).T
+  # print(all_cluster_ratio_list)
+  cluster_ratio_df = pd.DataFrame(all_cluster_ratio_list,columns=cluster_proportions.keys(),index = range_string(num_clusters))
   return cluster_ratio_df, ranges
 
-def cluster_eval_by_cluster(df):
+
+def range_string(num_clusters):
   max_days = 15
-  num_clusters = len(df['cluster'].unique())
   step_size = int(max_days/ num_clusters)
-  ranges = [{'min': i * step_size, 'max': (i + 1) * step_size} for i in range(num_clusters)]
-  ranges_str = [f"min: i * step_size, max: (i + 1) * step_size" for i in range(num_clusters)]
-  cluster_proportions = { cluster: [0] * len(ranges) for cluster in range(num_clusters) }
-  print(ranges)
-  for ck in cluster_proportions.keys(): # for each cluster
-    for idx, r in enumerate(ranges): # for each possible range
-      # we check how many that are in this cluster and fit the range
-      # print(df[df['cluster'] == ck])
-      cluster_proportions[ck][idx] = len(df[(df['cluster'] == ck) & df['days_since'].between(r['min'], r['max'])])
-      
-    # df['wrong_cluster'] = df.apply(lambda entry: entry in range(ranges[entry['cluster']]['min'], ranges[entry['cluster']]['max']), axis=1)
+  return [f"{i * step_size} to {(i + 1) * step_size}" for i in range(num_clusters)]
+
+
+def hist_plot_cluster_proportions(df):
+  bins = range_string(len(df.columns))
+  print(bins)
+  df.plot(kind='bar', rot=45)
+  plt.show()
+
+def to_percent_in_cluster(df):
+  new_df = df.copy()
+  # for cluster in new_df.columns:
+  #   total = new_df[cluster].sum()
+  #   for r in new_df.iterrows():
+  #     print(r)
+  #     df.loc[r][cluster] = df.loc[r][cluster] / total
+  return new_df
+
+def scatter_plot(df,filename):
+  plt.scatter(df, x = 'r-i', y = 'g-r', c = 'cluster')
+  return plt.show()
+
+def heatmap_proportions(clusters_evaluations,filename):
+  num_clusters = len(clusters_evaluations.columns)
+  fig, ax = plt.subplots()
+  print(len(clusters_evaluations.index))
+  im = ax.imshow(clusters_evaluations)
+
+  # Show all ticks and label them with the respective list entries
+  print([n for n in range(0,num_clusters)])
+  rows = [int(rng.split(' ')[-1]) for rng in clusters_evaluations.index]
+  columns = [n for n in range(0,num_clusters)]
+  print(rows, len(rows))
+  ax.set_xticks([int(cluster) for cluster in columns], labels= columns)
+  ax.set_yticks(range(len(rows)),labels = rows) #[row/num_clusters for row in rows]
+  ax.set_ylabel('Ranges')
+  ax.set_xlabel('Cluster')
+  print(ax.get_xticks(),ax.get_yticks())
+  # Rotate the tick labels and set their alignment.
+  plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+          rotation_mode="anchor")
   
-  all_cluster_ratio_list = [cluster_proportions[c] for c in cluster_proportions.keys()]
-  cluster_ratio_df = pd.DataFrame(all_cluster_ratio_list,columns=cluster_proportions.keys(),index = ranges)
-  return cluster_ratio_df, ranges
 
-def evaluation_to_df(evaluation):
-  pass
-
-
-def plot_cluster_proportions(clusters_evaluations):
-
-  pass
-
+  ax.set_title(f'Points in Range per Cluster\n{num_clusters} Clusters, {filename}')
+  fig.tight_layout()
+  return plt.show()
 
 if __name__ == '__main__':
   filename='two_day_range.csv'
-  num_clusters=7
+  num_clusters=5
   df = cluster_df(filename, num_clusters)
-  print(len(df.index))
-  cluster_ratio_df, ranges = cluster_eval(df)
+  # print(len(df.index))
+  cluster_df, ranges = cluster_eval(df)
+  cluster_ratio_df = to_percent_in_cluster(cluster_df)
   print(cluster_ratio_df)
-  plt.imshow(cluster_ratio_df)
-  plt.xlabel('cluster number')
-  plt.ylabel('day range number')
-  plt.colorbar()
-  plt.title(f'{num_clusters} Clusters, {filename}')
+  heatmap_proportions(cluster_ratio_df,filename)
+  hist_plot_cluster_proportions(cluster_ratio_df)
+  scatter_plot(df,filename)
   plt.show()
+  
